1937b74fc26998b6a9b07b6d48a60407f07f50b7
RaLLy08 <uytemataaero@mail.ru>
2023-12-23 01:44:17 +0400

    badParentSurvivePercent added

--- gasin.js
+++ gasin.js
@@ -1,6 +1,12 @@
-const originalSineParameters = [0.5, 5, 30]; // amplitude, frequency, phase
+ // amplitude, frequency, phase
+
+const originalSineParameters = [
+    0.5 + 2 * Math.random(), 
+    10 * Math.random(),
+    10 + Math.random() * 20
+];
 const noiseSineParams = [
-    0.5 + Math.random() * 0.5,
+    0.5 + Math.random() * 2,
     5 + Math.random() * 15,
     2 + Math.random() * 10
 ];
@@ -25,17 +31,17 @@ const views = {
 }
 
 views.originalSine.forEach((view, i) => {
-    view.innerHTML = originalSineParameters[i];
+    view.innerHTML = originalSineParameters[i].toFixed(3);
 })
 
 views.noisedSine.forEach((view, i) => {
-    view.innerHTML = noiseSineParams[i];
+    view.innerHTML = noiseSineParams[i].toFixed(3);
 })
 
 
-
-const updateView = (amplitude, frequency, phase, generation) => {
+const updateView = (amplitude, frequency, phase, bestFitness, generation) => {
     document.getElementById('generation').innerHTML = generation;
+    document.getElementById('best-fitness').innerHTML = Number(bestFitness).toFixed(3);
 
     views.targetSine.forEach((view, i) => {
         view.innerHTML = [amplitude.toFixed(3), frequency.toFixed(3), phase.toFixed(3)][i];
@@ -46,7 +52,7 @@ const mapRange = (toMin, toMax, fromMin, fromMax) => value => {
     return (value - fromMin) * (toMax - toMin) / (fromMax - fromMin) + toMin;
 }
 
-const dataXLength = 1000;
+const dataXLength = 800;
 
 const getSine = (t) => (amplitude, frequency, phase) => {
     return amplitude * Math.sin(
@@ -124,7 +130,7 @@ if (typeof window !== 'undefined') {
     const yBlackData = noisedDataY;
 
     // Declare the chart dimensions and margins.
-    const width = 1080;
+    const width = 1480;
     const height = 500;
     const marginTop = 60;
     const marginRight = 20;
@@ -226,16 +232,29 @@ const createInitialPopulation = (populationSize, genomeLength) => {
     return population;
 }
 
-const selection = (population, surviveSize) => {
+const selection = (population, surviveSize, badParentSurvivePercent) => {
     const sorted = population.sort((a, b) => fitness(a) - fitness(b));
 
-    return sorted.slice(0, surviveSize);
+    const badSurviveSize = Math.floor(population.length * badParentSurvivePercent);
+    const lastBad = sorted.slice(-badSurviveSize);
+
+    const badSurvive = [];
+
+    for (let i = 0; i < badSurviveSize; i++) {
+        const index = Math.floor(Math.random() * lastBad.length);
+
+        badSurvive.push(lastBad[index]);
+
+        lastBad.splice(index, 1);
+    }
+
+    return [
+        ...sorted.slice(0, surviveSize),
+        ...badSurvive
+    ];
 }
 
 const reduction = (population, surviveSize) => {
-//   for (let i = eliteSize; i < population.length - surviveSize; i++) {
-//     population.splice(Math.floor(Math.random() * population.length), 1);
-//   }
     population.splice(surviveSize, population.length);
 }
 
@@ -265,7 +284,7 @@ const nextGeneration = (parents, mutationRate, eliteSize) => {
     const newPopulation = [];
 
     for (let j = 0; j <= parents.length - 1; j++) {
-        const parentA = parents[j];
+        const parentA = parents[Math.floor(Math.random() * parents.length)];
         const parentB = parents[Math.floor(Math.random() * parents.length)];
         // const parentC = parents[Math.floor(Math.random() * parents.length)];
         // const parentD = parents[Math.floor(Math.random() * parents.length)];
@@ -311,45 +330,57 @@ const displayDenoisedWave = (bestGenome) => {
     );
 }
 
+let runCount = 0;
 
-const run = async (maxGenerations, populationSize, mutationRate, elite, parentSurvivePercent) => {
+const run = async (
+    maxGenerations, 
+    populationSize, 
+    mutationRate, 
+    elite, 
+    parentSurvivePercent, 
+    badParentSurvivePercent
+) => {
     let population = createInitialPopulation(populationSize, 3);
 
-    if (parentSurvivePercent < 0.5) {
-        throw new Error('parentSurvivePercent must be more than 0.5, becuse we return 2 children from 2 parents');
+    if (parentSurvivePercent + badParentSurvivePercent < 0.5) {
+        throw new Error('parent Survive Percent must be more than 0.5, becuse we return 2 children from 2 parents');
     }
 
+    if (parentSurvivePercent + badParentSurvivePercent > 1) {
+        throw new Error('parent Survive Percent + bad Parent Survive Percent must be less than 1');
+    }
+
+    const currentRun = runCount;
+
     for (let i = 0; i < maxGenerations; i++) {
+        if (currentRun !== runCount) {
+            return;
+        }
+
+
         await new Promise(resolve => setTimeout(resolve, 0));
         displayDenoisedWave(population[0])
 
         // will remove worst 20% amount of parents
 
-        const parents = selection(population, population.length * parentSurvivePercent);
+        const parents = selection(population, population.length * parentSurvivePercent, badParentSurvivePercent);
 
-        console.log(parents.length, 'selected parents')
         const eliteSize = elite * population.length;
 
         // will create children from parentSurvivePercent% parents 
         // dont mutate elite
-
         const newGeneration = nextGeneration(parents, mutationRate, eliteSize);
+
 
         population = [
             ...parents,
             ...newGeneration
         ];
-
         reduction(population, populationSize);
-    
-        console.log(`Generation: ${i} | Fitness: ${fitness(population[0])} | result: ${population[0]}`);
 
-        updateView(...population[0], i);
+        // console.log(`Generation: ${i} | Fitness: ${fitness(population[0])} | result: ${population[0]}`);
 
-        if (fitness(parents[0]) === 0) {
-            console.log(`Best genome: ${parents[0]}, result: ${originalEquation(...parents[0])}`);
-            return;
-        }
+        updateView(...population[0], fitness(population[0]), i);
     }
 
   if (typeof window !== 'undefined') {
@@ -362,28 +393,22 @@ const run = async (maxGenerations, populationSize, mutationRate, elite, parentSu
 
 run(
     maxGenerations = 1000,
-    populationSize = 140,
-    mutationRate = 0.3,
+    populationSize = 200,
+    mutationRate = 0.4,
     elite = 0.1,
-    parentSurvivePercent=0.5
+    parentSurvivePercent=0.4,
+    badParentSurvivePercent=0.3
 )
 
-// const a = [
-//     fitness([-0.5, 20, -6]), // 0
-//     fitness([-0.5, 20, -3]), // 1
-//     fitness([-0.5, 20, -2]), // 2
+restart.onclick = () => { 
+    runCount++;
 
-//     fitness([-0.5, 20, -1]), // 3
-//     fitness([-0.5, 20, -1.1]), // 4 better why
-
-//     fitness([-0.5, 20, 1]),
-//     fitness([-0.5, 20, 4]),
-//     fitness([-0.5, 20, 6]),
-//     fitness([-0.5, 20, 8]),
-//     fitness([-0.5, 20, 9]),
-//     fitness([-0.5, 20, 10]),
-// ]
-
-// console.log(a)
-
-
+    run(
+        maxGenerations = 1000,
+        populationSize = 160,
+        mutationRate = 0.4,
+        elite = 0.1,
+        parentSurvivePercent=0.4,
+        badParentSurvivePercent=0.3
+    )
+}
